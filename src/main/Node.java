b9d161a69b7ae718c280746508f929bcc1d229a0
sulabh2000 <bansalsulabh13@gmail.com>
2019-02-20 11:16:08 +0530

    add getter and setter for next

--- src/main/Node.java
+++ src/main/Node.java
@@ -10,5 +10,13 @@ public class Node {
     public void setData(int data) {
         this.data = data;
     }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
 
