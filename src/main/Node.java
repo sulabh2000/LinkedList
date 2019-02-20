369c8f79c3aefc4718c75c4c8d2356507d468427
sulabh2000 <bansalsulabh13@gmail.com>
2019-02-20 11:07:46 +0530

    add getter and setter for attribute data

--- src/main/Node.java
+++ src/main/Node.java
@@ -3,5 +3,12 @@ package main;
 public class Node {
         private int data;
 
+    public int getData() {
+        return data;
     }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
 
